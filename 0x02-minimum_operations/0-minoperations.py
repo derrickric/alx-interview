8f0a6fdbe2630fc0ffc9b5f84eeeb7478e9af9e5
derrickric <derrickmbaka@gmail.com>
2023-05-08 03:00:00 +0000

    minor chamges

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,20 @@
-#!/usr/bin/env python3
-"""Module for minOperations"""
+#!/usr/bin/python3
+"""
+Contains a method that calculates the fewest number of operations
+needed to result in exactly n H characters in the file.
+"""
 
 
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed to result in exactly n H characters in the file.
+    Returns the fewest number of operations needed to result in exactly
+    n H characters in the file.
     """
-    if n <= 1:
-        return 0
-    factors = []
-    i = 2
-    while i <= n:
-        while n % i == 0:
-            factors.append(i)
-            n = n // i
-        i += 1
-    if not factors:
-        return 0
-    return sum(factors)
-
+    operations = 0
+    min_operations = 2
+    while n > 1:
+        while n % min_operations == 0:
+            operations += min_operations
+            n /= min_operations
+        min_operations += 1
+    return operations
